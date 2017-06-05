42ba1f1ec62fbf2b297b8af655158fd6ee8fd03e
vihangab <bare.vihanga5@gmail.com>
2017-06-05 09:34:01 -0600

    modified run.py to include multithreading

--- webserver/flask/run.py
+++ webserver/flask/run.py
@@ -1,5 +1,17 @@
 # run.py
 
+# Author  : Vihanga Bare #
+
+import socket
+import os
+import threading
+import sys
+import select
+from signal import SIGINT
+
+port = 8888
+
+
 import os
 
 from app import create_app
@@ -8,4 +20,5 @@ config_name = os.getenv('FLASK_CONFIG')
 app = create_app(config_name)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
+    
