72f9efc7e067ec926e1bf574c1866a8d6fed814b
vihangab <bare.vihanga5@gmail.com>
2017-08-02 18:29:55 -0600

    client file for udp hole punching

--- client_info_updater/pythonclient.py
+++ client_info_updater/pythonclient.py
@@ -8,22 +8,22 @@ import sys
 aws_ip=("184.72.215.211",15000)
 
 try:
-    sockfd = socket.socket(socket.AF_NET,socket.SOCK_DGRAM)
-    sockfd.bind = ('',0)
-    sockfd.sento("hello",aws_ip)
+    sockfd = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    sockfd.bind(('',0))
+    sockfd.sendto("hello",aws_ip)
 
 except socket.error:
     print "failed to create socket" + str(socket.error)
     sys.exit()
 
-peer_data,addr = sockfd.recfrom(1024)
+peer_data,addr = sockfd.recvfrom(1024)
 print peer_data
 
 print "trying to connect to peer"
 peer_ip = peer_data.split(':')[0]
 peer_port = peer_data.split(':')[1]
 
-for i in the range 100:
+for i in range(100):
     print "sending packets"
     sockfd.sendto("hello from client A , ",(peer_ip,peer_port))
     time.sleep(1)
